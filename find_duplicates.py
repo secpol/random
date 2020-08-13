2fbd792334617f60311134e9c393dd6d42664199
pablos <pawel@ansible-lab.local>
2020-08-13 18:39:50 -0400

    Fourth

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,14 +1,15 @@
 import os, sys
 import os.path
-from collections import defaultdict
 from itertools import repeat
 from operator import itemgetter
+import time
 
-sys.argv.append('M:\\photos\\')
+sys.argv.append('E:\\')
 count = 0
 count2 = 0
 flist = []
 tohash = []
+start = time.time()
 
 temp = os.walk(sys.argv[1], topdown=False)
 
@@ -28,15 +29,16 @@ for a in range(len(flist)-1):
             header=chunk.read(1024)
         nlist.append(header)
         tohash.append(nlist)
-
         count+=1
-
-        print('file {} = {}'.format(flist[::1][a][0], flist[::1][a+1][0]))
-
+        print('\r' + 'Equal size files: {}'.format(str(count)), end='')
+        #print('file {} = {}'.format(flist[::1][a][0], flist[::1][a+1][0]))
 for b in range(len(tohash)-1): 
     if tohash[::1][b][1] == tohash[::1][b+1][1]:
-        print('found chunk {} is equal to this file {}'.format(tohash[::1][b][0], tohash[::1][b+1][0]))
         count2+=1
-
-print('total dups based on file size - {}'.format(count))
-print('total dups based on first 1024 bytes = {}'.format(count2))
+        print('\r' + 'Equal chunk size files: {}'.format(str(count2)), end='')
+        #print('found chunk {} is equal to this file {}'.format(tohash[::1][b][0], tohash[::1][b+1][0]))
+        
+#msec = round(1000 / avg, 4)
+print('\nTotal duplicates based on file size - {}'.format(count))
+print('Total duplicates based on first 1024 bytes - {}'.format(count2))
+print('Found in {} seconds'.format(round(time.time()-start, 2)))

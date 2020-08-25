324c04c0f23e81eb63706c3606cda59501487afb
pablos <pawel@ansible-lab.local>
2020-08-25 05:58:40 -0400

    Sixth - fixed compare() function

--- find_duplicates.py
+++ find_duplicates.py
@@ -5,14 +5,36 @@ from operator import itemgetter
 import hashlib
 import time
 
+sys.argv.append('M:\\photos\\')
+flist=[]
+start=time.time()
+temp=os.walk(sys.argv[1], topdown=False)
 
-sys.argv.append('E:\\photos\\')
+#compare second indexes from nested list and create potential list of duplicates
+def compare(some_list):
+    nlist=[]
+    nlist.append([])
+    count=0
+    for a in range(len(some_list)-1):
+        if some_list[::1][a][1] == some_list[::1][a+1][1]:
+            count+=1
+            nlist[0].append(some_list[::1][a][0])
+            nlist[0].append(some_list[::1][a+1][0])
+    #nlist.sort()
+    removed = [list(set(lst)) for lst in nlist]
+    #print(nlist)    #[['E:\\photos\\IMG 1860 - Copy.jpg', 'E:\\photos\\IMG 1860.jpg', 'E:\\photos\\IMG 1873 - Copy.jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873.jpg', 'E:\\photos\\IMG 1826 - Copy.jpg', 'E:\\photos\\IMG 1826.jpg']]
+    return removed,count
 
-count2 = 0
-flist = []
-start = time.time()
-
-temp = os.walk(sys.argv[1], topdown=False)
+#merge file path with file header/hash(TODO)
+def merge(another_list):
+    merge = []
+    for t in range(len(another_list[0])):
+        merge_row = []
+        for row in another_list:
+            merge_row.append(row[t])
+        merge.append(merge_row)
+    merge.sort(key=itemgetter(1))
+    return merge
 
 for root, dirs, files in temp:
     for i in files:
@@ -21,38 +43,14 @@ for root, dirs, files in temp:
         fsize.append(os.stat(os.path.join(root,i)).st_size)
         flist.append(fsize)
 flist.sort(key=itemgetter(1))
-#print (flist)   #[['E:\\photos\\IMG 1826.jpg', 49818], ['E:\\photos\\IMG 1833.jpg', 49353]...]
 
-#compare second indexes from nested list and create potential list of duplicates
-def compare(some_list):
-    nlist = []
-    nlist.append([])
-    index_num = -1
-    for a in range(len(some_list)-1):
-        if some_list[::1][a][1] == some_list[::1][a+1][1]:
-            index_num+=1
-            nlist[0].append(some_list[::1][a][0])
-            nlist[0].append(some_list[::1][a+1][0])
-    #print(nlist)    #[['E:\\photos\\IMG 1860 - Copy.jpg', 'E:\\photos\\IMG 1860.jpg', 'E:\\photos\\IMG 1873 - Copy.jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873.jpg', 'E:\\photos\\IMG 1826 - Copy.jpg', 'E:\\photos\\IMG 1826.jpg']]
-    return nlist
+tsts = compare(flist)[0]
+tsts.append([])
+for c in range(len(tsts[0])):
+    with open (tsts[0][c], "rb") as chunk:
+        header=chunk.read(1024)
+    tsts[1].append(header)
 
-lister = compare(flist)
-
-lister.append([])
-for c in range(len(lister[0])):
-    with open (lister[0][c], "rb") as chunk:
-        header=chunk.read(10)
-    lister[1].append(header)
-#print(lister)
-
-tohash = []
-for g in range(len(lister[0])):
-    tohash_row = []
-    for row in lister:
-        tohash_row.append(row[g])
-    tohash.append(tohash_row)
-print(tohash)
-
-print('\nTotal duplicates based on file size - {}'.format('TODO'))
-print('Total duplicates based on first 1024 bytes - {}'.format('TODO'))
+print('\nTotal duplicates based on file size - {}'.format(compare(flist)[1]))
+print('Total duplicates based on first 1024 bytes - {}'.format(compare(merge(tsts))[1]))
 print('Found in {} seconds'.format(round(time.time()-start, 2)))

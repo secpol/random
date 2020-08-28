b35a55e14f899b77e991202f88cf3ce9ba1de199
pablos <pawel@ansible-lab.local>
2020-08-28 17:21:21 -0400

    Seventh - TODO fix memory error

--- find_duplicates.py
+++ find_duplicates.py
@@ -5,25 +5,28 @@ from operator import itemgetter
 import hashlib
 import time
 
-sys.argv.append('M:\\photos\\')
-flist=[]
-start=time.time()
-temp=os.walk(sys.argv[1], topdown=False)
-
 #compare second indexes from nested list and create potential list of duplicates
-def compare(some_list):
+def compare(some_list, chunk_hash: bool):
     nlist=[]
     nlist.append([])
-    count=0
     for a in range(len(some_list)-1):
-        if some_list[::1][a][1] == some_list[::1][a+1][1]:
-            count+=1
+        if some_list[::1][a][1] == some_list[::1][a+1][1]: 
             nlist[0].append(some_list[::1][a][0])
             nlist[0].append(some_list[::1][a+1][0])
-    #nlist.sort()
-    removed = [list(set(lst)) for lst in nlist]
+    rem_dupas = [list(set(lst)) for lst in nlist] #remove duplicate paths
     #print(nlist)    #[['E:\\photos\\IMG 1860 - Copy.jpg', 'E:\\photos\\IMG 1860.jpg', 'E:\\photos\\IMG 1873 - Copy.jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873(2).jpg', 'E:\\photos\\IMG 1873.jpg', 'E:\\photos\\IMG 1826 - Copy.jpg', 'E:\\photos\\IMG 1826.jpg']]
-    return removed,count
+    rem_dupas.append([])
+    for b in range(len(rem_dupas[0])):
+        with open (rem_dupas[0][b], "rb") as data:
+            #print(rem_dupas[0][b])
+            if chunk_hash:
+                output=data.read(1024)
+            else:
+                data1=data.read()
+                output=hashlib.sha1(data1).hexdigest()
+        rem_dupas[1].append(output)
+
+    return rem_dupas
 
 #merge file path with file header/hash(TODO)
 def merge(another_list):
@@ -36,6 +39,11 @@ def merge(another_list):
     merge.sort(key=itemgetter(1))
     return merge
 
+sys.argv.append('M:\\docs\\')
+flist=[]
+start=time.time()
+temp=os.walk(sys.argv[1], topdown=False)
+
 for root, dirs, files in temp:
     for i in files:
         fsize = []
@@ -44,13 +52,8 @@ for root, dirs, files in temp:
         flist.append(fsize)
 flist.sort(key=itemgetter(1))
 
-tsts = compare(flist)[0]
-tsts.append([])
-for c in range(len(tsts[0])):
-    with open (tsts[0][c], "rb") as chunk:
-        header=chunk.read(1024)
-    tsts[1].append(header)
 
-print('\nTotal duplicates based on file size - {}'.format(compare(flist)[1]))
-print('Total duplicates based on first 1024 bytes - {}'.format(compare(merge(tsts))[1]))
+print('\nTotal duplicates based on file size - {}'.format(len(compare(flist, True)[0])))
+print('Total duplicates based on first 1024 bytes - {}'.format(len(merge(compare(flist, True)))))
+print('Total duplicates based on hash - {}'.format(len(merge(compare(flist, False)))))
 print('Found in {} seconds'.format(round(time.time()-start, 2)))
